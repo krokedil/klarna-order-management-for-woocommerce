901c1c1bad482a5830703e425e373f81536a7756
Michael Bengtsson <michael@krokedil.se>
2018-04-27 11:18:33 +0200

    Added PHP version and Krokedil to useragent

--- includes/class-wc-klarna-order-management-request.php
+++ includes/class-wc-klarna-order-management-request.php
@@ -155,7 +155,7 @@ class WC_Klarna_Order_Management_Request {
 				'Authorization' => $this->get_klarna_authorization_header(),
 				'Content-Type'  => 'application/json',
 			),
-			'user-agent' => apply_filters( 'http_headers_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ) ) . ' - OM:' . WC_KLARNA_ORDER_MANAGEMENT_VERSION,
+			'user-agent' => apply_filters( 'http_headers_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ) ) . ' - OM:' . WC_KLARNA_ORDER_MANAGEMENT_VERSION . ' - PHP Version: ' . phpversion() . ' - Krokedil',
 			'method'     => $this->klarna_request_method,
 		);
 
