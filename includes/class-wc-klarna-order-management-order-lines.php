e66ffd0d0d8f3647ab09822a0501b60059b3d1f1
Montazar Al-Jaber <montazar@duck.com>
2022-04-27 17:27:12 +0200

    Pay for Order: send image and product URL

--- includes/class-wc-klarna-order-management-order-lines.php
+++ includes/class-wc-klarna-order-management-order-lines.php
@@ -182,7 +182,7 @@ class WC_Klarna_Order_Management_Order_Lines {
 	 * @return array
 	 */
 	public function process_order_item_product( $order_item, $order ) {
-		return array(
+		$order_line = array(
 			'reference'             => $this->get_item_reference( $order_item ),
 			'type'                  => $this->get_item_type( $order_item ),
 			'name'                  => $this->get_item_name( $order_item ),
@@ -193,6 +193,20 @@ class WC_Klarna_Order_Management_Order_Lines {
 			'total_discount_amount' => $this->get_item_discount_amount( $order_item ),
 			'total_tax_amount'      => $this->get_item_tax_amount( $order_item ),
 		);
+
+		$settings = get_option( 'woocommerce_kco_settings', array() );
+		if ( isset( $settings['send_product_urls'] ) && 'yes' === $settings['send_product_urls'] ) {
+			$product = wc_get_product( $order_item->get_product_id() );
+
+			$image_url = $this->get_item_image_url( $product );
+			if ( $image_url ) {
+				$order_line['image_url'] = $image_url;
+			}
+
+			$order_line['product_url'] = $this->get_item_product_url( $product );
+		}
+
+		return $order_line;
 	}
 
 	/**
@@ -547,6 +561,35 @@ class WC_Klarna_Order_Management_Order_Lines {
 	public function get_item_type( $order_line_item ) {
 		$product = $order_line_item->get_product();
 		return $product && ! $product->is_virtual() ? 'physical' : 'digital';
+	}
+
+	/**
+	 * Get cart item product URL.
+	 *
+	 * @since  1.1
+	 * @access public
+	 *
+	 * @param  WC_Product $product Product.
+	 * @return string|false $item_product_url Cart item product URL.
+	 */
+	private function get_item_product_url( $product ) {
+		return $product->get_permalink();
+	}
+
+	/**
+	 * Get cart item product image URL.
+	 *
+	 * @param WC_Product $product
+	 * @return string|false $item_product_image_url Cart item product image URL.
+	 */
+	private function get_item_image_url( $product ) {
+		$image_url = false;
+		if ( $product->get_image_id() > 0 ) {
+			$image_id  = $product->get_image_id();
+			$image_url = wp_get_attachment_image_url( $image_id, 'shop_single', false );
+		}
+
+		return $image_url;
 	}
 
 }
