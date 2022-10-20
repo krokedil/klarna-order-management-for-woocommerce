2a1cfce8cd43bff55dd0dbd54421dd6450d81ccf
Montazar Al-Jaber <github.v0its@slmail.me>
2022-10-20 16:56:01 +0200

    Add filter for changing PW gift card SKU

--- classes/class-wc-klarna-order-management-order-lines.php
+++ classes/class-wc-klarna-order-management-order-lines.php
@@ -176,12 +176,13 @@ class WC_Klarna_Order_Management_Order_Lines {
 		 * PW WooCommerce Gift Cards.
 		 */
 		foreach ( $order->get_items( 'pw_gift_card' ) as $gift_card ) {
-			$gift_card_sku    = $gift_card->get_name();
+			$code             = $gift_card->get_card_number();
+			$gift_card_sku    = apply_filters( 'klarna_pw_gift_card_sku', __( 'gift_card', 'klarna-order-management-for-woocommerce' ), $code );
 			$gift_card_amount = intval( $gift_card->get_amount() * -100 );
 			$order_item       = array(
 				'type'                  => 'gift_card',
 				'reference'             => $gift_card_sku,
-				'name'                  => __( 'Gift card', 'pw-woocommerce-gift-cards' ) . ' ' . $gift_card_sku,
+				'name'                  => __( 'Gift card', 'pw-woocommerce-gift-cards' ) . ' ' . $code,
 				'quantity'              => 1,
 				'tax_rate'              => 0,
 				'total_discount_amount' => 0,
