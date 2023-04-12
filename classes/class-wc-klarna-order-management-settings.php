4e7b50692db2f4dbbaf8e4c0e42c2cec1d6ad6d8
MichaelBengtsson <michael@krokedil.se>
2023-04-12 11:44:05 +0200

    Change how to get default values to respect old settings if they exist

--- classes/class-wc-klarna-order-management-settings.php
+++ classes/class-wc-klarna-order-management-settings.php
@@ -22,6 +22,15 @@ class WC_Klarna_Order_Management_Settings {
 	}
 
 	public function extend_settings( $settings ) {
+		$default_values = get_option( 'kom_settings', array(
+			'kom_auto_capture' => 'yes',
+			'kom_auto_cancel' => 'yes',
+			'kom_auto_update'  => 'yes',
+			'kom_auto_order_sync'  => 'yes',
+			'kom_force_full_capture'  => 'no',
+			'kom_debug_log'  => 'yes',
+		) );
+
 		$settings['kom'] = array(
 			'title' => 'Klarna Order Management',
 			'type'  => 'title',
@@ -30,42 +39,42 @@ class WC_Klarna_Order_Management_Settings {
 		$settings['kom_auto_capture'] = array(
 			'title'   => 'On order completion',
 			'type'    => 'checkbox',
-			'default' => 'yes',
+			'default' => $default_values['kom_auto_capture'],
 			'label'   => __( 'Activate Klarna order automatically when WooCommerce order is marked complete.', 'klarna-order-management-for-woocommerce' ),
 		);
 
 		$settings['kom_auto_cancel'] = array(
 			'title'   => 'On order cancel',
 			'type'    => 'checkbox',
-			'default' => 'yes',
+			'default' => $default_values['kom_auto_cancel'],
 			'label'   => __( 'Cancel Klarna order automatically when WooCommerce order is marked canceled.', 'klarna-order-management-for-woocommerce' ),
 		);
 
 		$settings['kom_auto_update'] = array(
 			'title'   => 'On order update',
 			'type'    => 'checkbox',
-			'default' => 'yes',
+			'default' => $default_values['kom_auto_update'],
 			'label'   => __( 'Update Klarna order automatically when WooCommerce order is updated.', 'klarna-order-management-for-woocommerce' ),
 		);
 
 		$settings['kom_auto_order_sync'] = array(
 			'title'   => 'On order creation ( manual )',
 			'type'    => 'checkbox',
-			'default' => 'yes',
+			'default' => $default_values['kom_auto_order_sync'],
 			'label'   => __( 'Gets the customer information from Klarna when creating a manual admin order and adding a Klarna order id as a transaction id.', 'klarna-order-management-for-woocommerce' ),
 		);
 
 		$settings['kom_force_full_capture'] = array(
 			'title'   => 'Force capture full order',
 			'type'    => 'checkbox',
-			'default' => 'no',
+			'default' => $default_values['kom_force_full_capture'],
 			'label'   => __( 'Force capture full order. Useful if the Klarna order has been updated by an ERP system.', 'klarna-order-management-for-woocommerce' ),
 		);
 
 		$settings['kom_debug_log'] = array(
 			'title'   => 'Debug log',
 			'type'    => 'checkbox',
-			'default' => 'yes',
+			'default' => $default_values['kom_debug_log'],
 			'label'   => __( 'Enable the debug log.', 'klarna-order-management-for-woocommerce' ),
 		);
 
