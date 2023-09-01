95dac752b6329caf355c688adff69738b1dd8df2
Montazar Al-Jaber <github.v0its@slmail.me>
2023-09-01 10:55:14 +0200

    Add HPOS helper functions

--- includes/klarna-order-management-functions.php
+++ includes/klarna-order-management-functions.php
@@ -9,6 +9,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
+
+/**
+ * Equivalent to WP's get_the_ID() with HPOS support.
+ *
+ * @return int the order ID or false.
+ */
+//phpcs:ignore
+function kom_get_the_ID() {
+	$hpos_enabled = kom_is_hpos_enabled();
+	$order_id     = $hpos_enabled ? filter_input( INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT ) : get_the_ID();
+	if ( empty( $order_id ) ) {
+		return false;
+	}
+
+	return $order_id;
+}
+
+/**
+ * Whether HPOS is enabled.
+ *
+ * @return bool
+ */
+function kom_is_hpos_enabled() {
+	// CustomOrdersTableController was introduced in WC 6.4.
+	if ( class_exists( CustomOrdersTableController::class ) ) {
+		return wc_get_container()->get( CustomOrdersTableController::class )->custom_orders_table_usage_is_enabled();
+	}
+
+	return false;
+}
+
 /**
  * Get the product and its image URLs.
  *
