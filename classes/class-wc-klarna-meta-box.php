422a6c1c6c70e9756e1c74875af03563f6575794
anyakro <anya@krokedil.se>
2025-03-06 15:41:38 +0100

    Remove unused method

--- classes/class-wc-klarna-meta-box.php
+++ classes/class-wc-klarna-meta-box.php
@@ -439,52 +439,5 @@ class WC_Klarna_Meta_Box {
 			WC_Klarna_Sellers_App::populate_klarna_order( $post_id, $klarna_order );
 		}
 	}
-
-	/**
-	 * Display scheduled actions related to the order.
-	 *
-	 * @param string $session_id The session ID.
-	 * @return void
-	 */
-	public function display_scheduled_actions( $session_id ) {
-		$session_query_url = admin_url(
-			'admin.php?page=wc-status&tab=action-scheduler&s=' . rawurlencode( $session_id ) . '&action=-1&paged=1&action2=-1'
-		);
-		$statuses          = array( 'complete', 'failed', 'pending' );
-		$action_counts     = array();
-
-		foreach ( $statuses as $status ) {
-			$action_counts[ $status ] = count(
-				as_get_scheduled_actions(
-					array(
-						'search'   => $session_id,
-						'status'   => array( $status ),
-						'per_page' => -1,
-					)
-				)
-			);
-		}
-		?>
-		<strong>
-			<?php esc_html_e( 'Scheduled actions ', 'klarna-order-management-for-woocommerce' ); ?>
-			<span class="woocommerce-help-tip"
-					data-tip="<?php esc_html_e( 'See all actions scheduled for this order.', 'klarna-order-management-for-woocommerce' ); ?>">
-			</span>
-		</strong>
-		<br />
-		<a target="_blank" href="<?php echo esc_url( $session_query_url ); ?>">
-			<?php
-			printf(
-				// translators: %1$d: number of completed orders, %2$d: number of failed orders, %3$d: number of pending orders.
-				esc_html__( '%1$d completed, %2$d failed, %3$d pending', 'klarna-order-management-for-woocommerce' ),
-				esc_html( $action_counts['complete'] ),
-				esc_html( $action_counts['failed'] ),
-				esc_html( $action_counts['pending'] )
-			);
-			?>
-		</a>
-		<br />
-		<?php
-	}
 }
 new WC_Klarna_Meta_Box();
