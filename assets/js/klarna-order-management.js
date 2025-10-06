44d2b6ae4c9f8d1d0cd5c1f9655179b38ce3baa8
anyakro <anya@krokedil.se>
2025-10-06 11:29:27 +0200

    Version 1.10.0
    * Feature - Added support for return fees on refunds; return fee is added as a fee to the refund order and shown in admin, customer emails, and "My account" page.
    * Feature - Scheduled actions for the current order are now shown on the admin order view.
    * Enhancement - Added refund order id as reference in request to Klarna.

--- assets/js/klarna-order-management.js
+++ assets/js/klarna-order-management.js
@@ -13,81 +13,82 @@ jQuery( function ( $ ) {
 			sync_status: function () {
 				return kom.toggle_button.hasClass( "woocommerce-input-toggle--enabled" ) ? "enabled" : "disabled"
 			},
-			show_refund_fee_section: function() {
+			show_refund_fee_section: function () {
 				// Show the return fee section if it is hidden.
-				$klarnaReturnFee = $('#klarna_return_fee');
-				$klarnaReturnFee.show();
+				$klarnaReturnFee = $( "#klarna_return_fee" )
+				$klarnaReturnFee.show()
 			},
-			hide_refund_fee_section: function() {
+			hide_refund_fee_section: function () {
 				// Hide the return fee section if it is visible.
 				// If the return fee section is hidden, do nothing.
-				$klarnaReturnFee = $('#klarna_return_fee');
+				$klarnaReturnFee = $( "#klarna_return_fee" )
 
-				if ($klarnaReturnFee.attr('data-klarna-hide') === 'no' ) {
-					return;
+				if ( $klarnaReturnFee.attr( "data-klarna-hide" ) === "no" ) {
+					return
 				}
 
-				$klarnaReturnFee.hide();
+				$klarnaReturnFee.hide()
 			},
-			modify_refund_button_text: function() {
+			modify_refund_button_text: function () {
 				// Add a span with id klarna_return_fee_total to the button with class do-api-refund.
 				// This span will be used to display the return fee amount in the button text.
-				const $klarnaRefundButton = $("button.do-api-refund");
-				$klarnaRefundButton.append( '<span id="klarna_return_fee_total"></span>' );
+				const $klarnaRefundButton = $( "button.do-api-refund" )
+				$klarnaRefundButton.append( '<span id="klarna_return_fee_total"></span>' )
 			},
-			update_klarna_refund_amount: function() {
+			update_klarna_refund_amount: function () {
 				// Get the return fee amount and tax amount from the input fields.
 				// If the return fee amount is 0, do nothing.
-				const $klarnaReturnFeeAmountField = $('#klarna_return_fee input.refund_line_total.wc_input_price');
-				const $klarnaReturnFeeTaxAmountField = $('#klarna_return_fee input.refund_line_tax.wc_input_price');
-				const $klarnaReturnFeeTotalSpan = $("span#klarna_return_fee_total");
+				const $klarnaReturnFeeAmountField = $( "#klarna_return_fee input.refund_line_total.wc_input_price" )
+				const $klarnaReturnFeeTaxAmountField = $( "#klarna_return_fee input.refund_line_tax.wc_input_price" )
+				const $klarnaReturnFeeTotalSpan = $( "span#klarna_return_fee_total" )
 
-				const refundFeeAmount = kom.unformat_number($klarnaReturnFeeAmountField.val()) + kom.unformat_number($klarnaReturnFeeTaxAmountField.val());
+				const refundFeeAmount =
+					kom.unformat_number( $klarnaReturnFeeAmountField.val() ) +
+					kom.unformat_number( $klarnaReturnFeeTaxAmountField.val() )
 
-				if( refundFeeAmount === 0 ) {
-					return;
+				if ( refundFeeAmount === 0 ) {
+					return
 				}
 
 				// Update the button text with the return fee amount by replacing inner text of the span#klarna_return_fee_total with the refund fee amount.
-				$klarnaReturnFeeTotalSpan.text(' (' + kom_admin_params.with_return_fee_text + ' ' + kom.format_number(refundFeeAmount) + ')' );
+				$klarnaReturnFeeTotalSpan.text(
+					" (" + kom_admin_params.with_return_fee_text + " " + kom.format_number( refundFeeAmount ) + ")",
+				)
 			},
-			format_number: function (number) {
+			format_number: function ( number ) {
 				// Format the number using accounting.js with the currency format settings from woocommerce_admin_meta_boxes.
-				return accounting.formatMoney(
-					number,
-					{
-						symbol: woocommerce_admin_meta_boxes.currency_format_symbol,
-						decimal: woocommerce_admin_meta_boxes.currency_format_decimal_sep,
-						thousand: woocommerce_admin_meta_boxes.currency_format_thousand_sep,
-						precision: woocommerce_admin_meta_boxes.currency_format_num_decimals,
-						format: woocommerce_admin_meta_boxes.currency_format
-					}
-				);
+				return accounting.formatMoney( number, {
+					symbol: woocommerce_admin_meta_boxes.currency_format_symbol,
+					decimal: woocommerce_admin_meta_boxes.currency_format_decimal_sep,
+					thousand: woocommerce_admin_meta_boxes.currency_format_thousand_sep,
+					precision: woocommerce_admin_meta_boxes.currency_format_num_decimals,
+					format: woocommerce_admin_meta_boxes.currency_format,
+				} )
 			},
-			unformat_number: function (number) {
+			unformat_number: function ( number ) {
 				// Unformat the number using accounting.js with the decimal point from woocommerce_admin.
 				// This is used to convert the formatted number back to a float for calculations.
-				return accounting.unformat(
-					number,
-					woocommerce_admin.mon_decimal_point
-				);
+				return accounting.unformat( number, woocommerce_admin.mon_decimal_point )
 			},
-			on_refund_submit: function (e) {
+			on_refund_submit: function ( e ) {
 				// Get the refund amount from the input field.
-				const $refundAmount = $('#refund_amount');
-				const $klarnaReturnFeeAmountField = $('#klarna_return_fee input.refund_line_total.wc_input_price');
-				const $klarnaReturnFeeTaxAmountField = $('#klarna_return_fee input.refund_line_tax.wc_input_price');
+				const $refundAmount = $( "#refund_amount" )
+				const $klarnaReturnFeeAmountField = $( "#klarna_return_fee input.refund_line_total.wc_input_price" )
+				const $klarnaReturnFeeTaxAmountField = $( "#klarna_return_fee input.refund_line_tax.wc_input_price" )
 
-				const diff = kom.unformat_number($refundAmount.val()) - (kom.unformat_number($klarnaReturnFeeAmountField.val()) + kom.unformat_number($klarnaReturnFeeTaxAmountField.val()));
+				const diff =
+					kom.unformat_number( $refundAmount.val() ) -
+					( kom.unformat_number( $klarnaReturnFeeAmountField.val() ) +
+						kom.unformat_number( $klarnaReturnFeeTaxAmountField.val() ) )
 
-				if (diff < 0) {
+				if ( diff < 0 ) {
 					// Show an alert box with the message "Refund amount is less than the return fee amount."
-					window.alert(kom_admin_params.refund_amount_less_than_return_fee_text);
+					window.alert( kom_admin_params.refund_amount_less_than_return_fee_text )
 
 					// Pause the default action of the button.
-					e.preventDefault();
-					e.stopPropagation();
-					return;
+					e.preventDefault()
+					e.stopPropagation()
+					return
 				}
 			},
 		}
@@ -114,18 +115,18 @@ jQuery( function ( $ ) {
 		} )
 
 		kom.refund_items_button.on( "click", function () {
-			console.log('refund_items_button');
+			console.log( "refund_items_button" )
 			kom.show_refund_fee_section()
 		} )
 		kom.cancel_refund_button.on( "click", function () {
 			kom.hide_refund_fee_section()
 		} )
-		kom.refund_submit_button.on( "click", function (e) {
-			kom.on_refund_submit(e)
+		kom.refund_submit_button.on( "click", function ( e ) {
+			kom.on_refund_submit( e )
 		} )
 		kom.refund_amount_field.on( "change", function () {
 			kom.update_klarna_refund_amount()
 		} )
-		kom.modify_refund_button_text();
+		kom.modify_refund_button_text()
 	} )
 } )
--- klarna-order-management-for-woocommerce.php
+++ klarna-order-management-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Provides order management for Klarna Payments and Klarna Checkout gateways.
  * Author: klarna, krokedil
  * Author URI: https://krokedil.se/
- * Version: 1.9.9
+ * Version: 1.10.0
  * Text Domain: klarna-order-management-for-woocommerce
  * Domain Path: /languages
  *
  * WC requires at least: 5.0.0
- * WC tested up to: 9.9.4
+ * WC tested up to: 10.2.2
  *
  * Copyright (c) 2018-2025 Krokedil
  *
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'WC_KLARNA_ORDER_MANAGEMENT_VERSION', '1.9.9' );
+define( 'WC_KLARNA_ORDER_MANAGEMENT_VERSION', '1.10.0' );
 define( 'WC_KLARNA_ORDER_MANAGEMENT_MIN_PHP_VER', '5.3.0' );
 define( 'WC_KLARNA_ORDER_MANAGEMENT_MIN_WC_VER', '3.3.0' );
 define( 'WC_KLARNA_ORDER_MANAGEMENT_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
